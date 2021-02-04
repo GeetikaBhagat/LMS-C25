98e0490ac0b750f9def87363efeb8cc691e85a06
GeetikaBhagat <68065986+GeetikaBhagat@users.noreply.github.com>
2021-02-04 17:47:31 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -7,27 +7,19 @@ class Dustbin {
         this.y = y;
         this.w = 200;
         this.h = 20;
-        this.angle = 0
 
         this.image = loadImage('dustbin.png')
         this.bottomB = Bodies.rectangle(this.x, this.y, this.w, this.h, options);
 
         this.leftB = Bodies.rectangle(this.x - this.w / 2, this.y - this.w / 2, this.h, this.w, options);
 
-        Matter.Body.setAngle(this.leftB, -1 * this.angle);
-
         this.rightB = Bodies.rectangle(this.x + this.w / 2, this.y - this.w / 2, this.h, this.w, options);
-
-        Matter.Body.setAngle(this.leftB, this.angle);
 
         World.add(world, [this.bottomB, this.leftB, this.rightB]);
 
     }
     show() {
         var posB = this.bottomB.position;
-        var posL = this.leftB.position;
-        var posR = this.rightB.position;
-
         push();
         translate(posB.x, posB.y)
         imageMode(CENTER);
